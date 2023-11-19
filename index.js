1e2ea91b62cdfea451919b774cacd750649224da
MC洛洛 <1592859065@qq.com>
2023-11-20 03:28:47 +0800

    还是p合适，给id1加了个自动隐藏

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ var specialDate = new Date(year, 10, 20);
 if (date.getDate() === specialDate.getDate() &&
     date.getMonth() === specialDate.getMonth() &&
     date.getFullYear() === specialDate.getFullYear()) {
+  document.getElementById("1").style.display = "none";
   document.getElementById("1120").style.display = "block";
 }
