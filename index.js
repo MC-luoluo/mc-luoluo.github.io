6f0aeb061c892e5a995401566734b1275d1f0264
MC洛洛 <1592859065@qq.com>
2024-02-19 16:05:32 +0800

    新加两个特定时间的文本

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var date = new Date;
 var year = date.getFullYear();
+var specialDate = new Date(year, 0, 22);
+if (date.getDate() === specialDate.getDate() &&
+    date.getMonth() === specialDate.getMonth() &&
+    date.getFullYear() === specialDate.getFullYear()) {
+  document.getElementById("1").style.display = "none";
+  document.getElementById("0122").style.display = "block";
+}
+var specialDate = new Date(year, 2, 27);
+if (date.getDate() === specialDate.getDate() &&
+    date.getMonth() === specialDate.getMonth() &&
+    date.getFullYear() === specialDate.getFullYear()) {
+  document.getElementById("1").style.display = "none";
+  document.getElementById("0327").style.display = "block";
+}
 var specialDate = new Date(year, 10, 20);
 if (date.getDate() === specialDate.getDate() &&
     date.getMonth() === specialDate.getMonth() &&
