e468c451eac8646a8401bcf9ac3778d6fa8a3172
MCluoluo <luoluo.moe@gmail.com>
2025-11-12 14:03:24 +0800

    update

--- en/index.js
+++
@@ -1,9 +0,0 @@
-var date = new Date;
-var year = date.getFullYear();
-var specialDate = new Date(year, 10, 20);
-if (date.getDate() === specialDate.getDate() &&
-    date.getMonth() === specialDate.getMonth() &&
-    date.getFullYear() === specialDate.getFullYear()) {
-  document.getElementById("1").style.display = "none";
-  document.getElementById("1120").style.display = "block";
-}
--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-var date = new Date;
-var year = date.getFullYear();
-var specialDate = new Date(year, 0, 22);
-if (date.getDate() === specialDate.getDate() &&
-    date.getMonth() === specialDate.getMonth() &&
-    date.getFullYear() === specialDate.getFullYear()) {
-  document.getElementById("1").style.display = "none";
-  document.getElementById("0122").style.display = "block";
+const date = new Date;
+if (date.getMonth() === 0 && date.getDate() === 22) {
+    document.getElementById("1").style.display = "none";
+    document.getElementById("0122").style.display = "block";
 }
-var specialDate = new Date(year, 2, 27);
-if (date.getDate() === specialDate.getDate() &&
-    date.getMonth() === specialDate.getMonth() &&
-    date.getFullYear() === specialDate.getFullYear()) {
-  document.getElementById("1").style.display = "none";
-  document.getElementById("0327").style.display = "block";
+if (date.getMonth() === 2 && date.getDate() === 27) {
+    document.getElementById("1").style.display = "none";
+    document.getElementById("0327").style.display = "block";
 }
-var specialDate = new Date(year, 10, 20);
-if (date.getDate() === specialDate.getDate() &&
-    date.getMonth() === specialDate.getMonth() &&
-    date.getFullYear() === specialDate.getFullYear()) {
-  document.getElementById("1").style.display = "none";
-  document.getElementById("1120").style.display = "block";
+if (date.getMonth() === 10 && date.getDate() === 20) {
+    document.getElementById("1").style.display = "none";
+    document.getElementById("1120").style.display = "block";
 }
