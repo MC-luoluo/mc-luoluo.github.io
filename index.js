83507d6cb5049746a00091f266fe5bb5cb8c3880
MCluoluo <luoluo.moe@gmail.com>
2025-11-20 20:31:02 +0800

    fix js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const date = new Date;
-if (date.getMonth() === 0 && date.getDate() === 22) {
-    document.getElementById("1").style.display = "none";
-    document.getElementById("0122").style.display = "block";
-}
-if (date.getMonth() === 2 && date.getDate() === 27) {
-    document.getElementById("1").style.display = "none";
-    document.getElementById("0327").style.display = "block";
-}
-if (date.getMonth() === 10 && date.getDate() === 20) {
-    document.getElementById("1").style.display = "none";
-    document.getElementById("1120").style.display = "block";
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const date = new Date;
+    if (date.getMonth() === 0 && date.getDate() === 22) {
+        document.getElementById("main").style.display = "none";
+        document.getElementById("0122").style.display = "block";
+    }
+    if (date.getMonth() === 2 && date.getDate() === 27) {
+       document.getElementById("main").style.display = "none";
+     document.getElementById("0327").style.display = "block";
+    }
+    if (date.getMonth() === 10 && date.getDate() === 20) {
+        document.getElementById("main").style.display = "none";
+        document.getElementById("1120").style.display = "block";
+    }
+});
